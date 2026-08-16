8c5ba3ef2b1334abb6ddaba351cdaab3adfc9805
hijarstories <hijarcorp@gmail.com>
2026-08-15 23:03:00 -0500

    check

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
-  title: '🐱 ¿Saldrías ckkonmigo? 🐱',
+  title: '🐱 ¿Saldrías coooonmigo? 🐱',
   description: 'Cita nerd con estilo meme y gatitos.',
 };
 
